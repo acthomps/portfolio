1732bdbb0156575bbce88be9abc3f2aaa306a693
Austin <>
2018-03-21 18:50:18 -0600

    Implemented persistent position updates for drag and drop interface

--- app/controllers/portfols_controller.rb
+++ app/controllers/portfols_controller.rb
@@ -7,6 +7,15 @@ class PortfolsController < ApplicationController
 		@portfolio_items = Portfol.by_position
 	end
 
+  def sort
+    params[:order].each do |key, value|
+      Portfol.find(value[:id]).update(position: value[:position])
+    end
+
+    render nothing: true
+  end
+
+
 	def angular
 		@angular_portfolio_items = Portfol.angular
 	end
